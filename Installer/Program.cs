50f09dad0f28193e027c3b830b7719e1a808406b
luca <luca@PORNHUB>
2024-01-15 23:53:24 +0100

    Add project files.

---
+++ Installer/Program.cs
@@ -0,0 +1,33 @@
+﻿using AsmResolver.DotNet;
+using AsmResolver.PE.DotNet.Cil;
+
+namespace Installer
+{
+    internal class Program
+    {
+        const string GAME_PATH = @"C:\Program Files (x86)\Steam\steamapps\common\Lethal Company\Lethal Company_Data\Managed";
+        const string DLL_PATH = @"C:\Users\luca\Documents\Projects\NonlethalCompany\NonlethalCompany\bin\Debug";
+
+        static void Main(string[] args)
+        {
+            if (File.Exists(GAME_PATH + @"\Assembly-CSharp.dll.bak"))
+                File.Copy(GAME_PATH + @"\Assembly-CSharp.dll.bak", GAME_PATH + @"\Assembly-CSharp.dll", true);
+            else
+                File.Copy(GAME_PATH + @"\Assembly-CSharp.dll", GAME_PATH + @"\Assembly-CSharp.dll.bak", true);
+            File.Copy(DLL_PATH + @"\NonlethalCompany.dll", GAME_PATH + @"\NonlethalCompany.dll", true);
+            File.Copy(DLL_PATH + @"\0Harmony.dll", GAME_PATH + @"\0Harmony.dll", true);
+
+            var gameImage = ModuleDefinition.FromFile(GAME_PATH + @"\Assembly-CSharp.dll");
+            var importer = new ReferenceImporter(gameImage);
+            var type = gameImage.GetAllTypes().Where(x => x.Name == "InitializeGame").First();
+            var method = type.Methods.Where(x => x.Name == "Start").First();
+
+            var dllImage = ModuleDefinition.FromFile(DLL_PATH + @"\NonlethalCompany.dll");
+            TypeDefinition typeToImport = dllImage.TopLevelTypes.First(t => t.Name == "Game");
+            MethodDefinition methodToImport = typeToImport.Methods.First(m => m.Name == "Initialize");
+            IMethodDefOrRef importedMethod = methodToImport.ImportWith(importer);
+            method.CilMethodBody.Instructions.Insert(0, new CilInstruction(CilOpCodes.Call, importedMethod));
+            gameImage.Write(GAME_PATH + @"\Assembly-CSharp.dll");
+        }
+    }
+}
---
+++ NonlethalCompany/Game.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+using GameNetcodeStuff;
+
+namespace NonlethalCompany
+{
+    [HarmonyPatch(typeof(PlayerControllerB), nameof(PlayerControllerB.AllowPlayerDeath))]
+    class GodModePatch
+    {
+        static bool Prefix()
+        {
+            Console.WriteLine("[Unlethal Company][God Mode] Calling GameNetcodeStuff::PlayerControllerB::AllowPlayerDeath() -> false");
+            return false;
+        }
+    }
+
+    public static class Game
+    {
+        public static void Initialize()
+        {
+            AllocConsole();
+            Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true });
+            Application.logMessageReceivedThreaded += (string condition, string stackTrace, LogType type) =>
+            {
+                //Console.WriteLine($"[Lethal Company][Log] {condition} {stackTrace}");
+            };
+
+            Console.WriteLine("[Unlethal Company] DLL loaded!");
+
+            var harmony = new Harmony("me.layle.nonlethalcompany");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [DllImport("kernel32")]
+        static extern bool AllocConsole();
+    }
+}
